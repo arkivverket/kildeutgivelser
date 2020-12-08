9042c1b550a041cc08290dc196c10cbc7f4289a6
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-12-08 10:11:18 +0100

    Remove reference to buildx in dockerfile

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker buildx build --tag kildeutgivelser/web --file Dockerfile.web .
-docker buildx build --tag kildeutgivelser/api_proxy --file Dockerfile.api_proxy .
-docker buildx build --tag kildeutgivelser/tei_tools --file Dockerfile.tei_tools .
-docker buildx build --tag kildeutgivelser/api --file Dockerfile.api .
+docker build --tag kildeutgivelser/web --file Dockerfile.web .
+docker build --tag kildeutgivelser/api_proxy --file Dockerfile.api_proxy .
+docker build --tag kildeutgivelser/tei_tools --file Dockerfile.tei_tools .
+docker build --tag kildeutgivelser/api --file Dockerfile.api .
