aea949355eb3c94c32b83fc5b3b6aee6ca0e956b
Ole-Martin Bratteng <ole-martin@bratteng.com>
2021-06-17 12:55:17 +0200

    SVN: add SVN image

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ docker build --tag kildeutgivelser/web --file Dockerfile.web .
 docker build --tag kildeutgivelser/tei_tools --file Dockerfile.tei_tools .
 docker build --tag kildeutgivelser/api --file Dockerfile.api .
 docker build --tag kildeutgivelser/pgsql --file Dockerfile.pgsql .
+docker build --tag kildeutgivelser/svn --file Dockerfile.svn .
