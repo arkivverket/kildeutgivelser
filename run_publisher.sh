8b96c59e06b94de35b77481ba96c2c75a4d6ceef
Ole-Martin Bratteng <ole-martin@bratteng.com>
2021-09-29 14:57:05 +0200

    Add svn_sync to cronjob

--- run_publisher.sh
+++ run_publisher.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+docker run --rm -it \
+    --env-file /srv/kildeutgivelser/.env \
+    -v svn_sync_cache:/tmp/svn_sync_cache \
+    kildeutgivelser/svn_sync
+
 docker exec kildeutgivelser_api python /app/sls_api/scripts/publisher.py stattholder --all_ids
 
 echo $(date +"%Y-%m-%dT%H:%M:%S%z") > /tmp/run_publisher.log
