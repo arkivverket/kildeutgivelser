e5f77ba10c8fd56f3092943262ce5aa0a67ff716
Ole-Martin Bratteng <ole-martin@bratteng.com>
2021-09-30 09:53:24 +0200

    connect svn_sync container to svn network

--- run_publisher.sh
+++ run_publisher.sh
@@ -3,6 +3,7 @@
 docker run --rm \
     --env-file /srv/kildeutgivelser/.env \
     -v svn_sync_cache:/tmp/svn_sync_cache \
+    --network kildeutgivelser_svn \
     kildeutgivelser/svn_sync
 
 docker exec kildeutgivelser_api python /app/sls_api/scripts/publisher.py stattholder --all_ids
