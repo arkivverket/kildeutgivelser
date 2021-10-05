e6672e5a261975bd2a2f78c34aa03f139be11e01
Ole-Martin Bratteng <ole-martin@bratteng.com>
2021-10-05 16:14:28 +0200

    cronjob: start a new docker container for running publisher script

--- run_publisher.sh
+++ run_publisher.sh
@@ -2,13 +2,19 @@
 
 # Runs sync between Edith and git
 docker run --rm \
-    --env-file /srv/kildeutgivelser/.env \
     -v svn_sync_cache:/tmp/svn_sync_cache \
+    --env-file /srv/kildeutgivelser/.env \
     --network kildeutgivelser_svn \
     kildeutgivelser/svn_sync
 
+
 # Runs the publisher script. Disregard that it says "stattholder", the name does not matter that much.
-docker exec kildeutgivelser_api python /app/sls_api/scripts/publisher.py stattholder --all_ids
+docker run --rm \
+    -v /srv/stattholder_files:/var/stattholder-files/ \
+    -v /srv/kildeutgivelser/api/config_templates:/config_templates \
+    --env-file /srv/kildeutgivelser/.env \
+	--network traefik \
+    kildeutgivelser/api python /app/sls_api/scripts/publisher.py stattholder --all_ids
 
 # Just tracking that it actually works and runs through fine
 echo $(date +"%Y-%m-%dT%H:%M:%S%z") > /tmp/run_publisher.log
